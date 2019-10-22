be8438e4e0a06901f35f42956e1840f02cbd9522
lachlan <lachlan.00@gmail.com>
2019-10-22 12:43:27 +1000

    Update example.py

--- example.py
+++ example.py
@@ -13,9 +13,10 @@ print('\n#######################\nTesting the ampache API\n#####################
 'handshake'
 """
 # processed details
+print('Connecting to', ampache_url)
 encrypted_key = ampache.encrypt_string(my_api_key, user)
 ampache_api   = ampache.handshake(ampache_url, encrypted_key)
-print('The ampache handshake for:\n    ', my_api_key, '\n\nReturned the following session key:\n    ', ampache_api)
+print('\nThe ampache handshake for:\n    ', my_api_key, '\n\nReturned the following session key:\n    ', ampache_api)
 
 """
 'ping'
@@ -61,13 +62,13 @@ album
 """
 for child in ampache.album(ampache_url, ampache_api, single_album, ''):
     if child.tag == 'album':
-        print('searching for an album with this id', single_album)
+        print('\nsearching for an album with this id', single_album)
         print(child.tag, child.attrib)
 
 """
 album_songs
 """
-print('\nsearching for songs with this that are in this album', single_album)
+print('\nsearching for songs that are in this album', single_album)
 for child in ampache.album_songs(ampache_url, ampache_api, single_album, '', 0):
     if child.tag == 'song':
         print(child.tag, child.attrib)
@@ -76,17 +77,34 @@ albums
 """
 
 """
+stats
+"""
+for child in ampache.stats(ampache_url, ampache_api, 'artist', '', 0, 1, 0, 0):
+    if child.tag == 'artist':
+        print('\ngetting a random artist using the stats method and found', child.attrib['id'])
+        single_artist = child.attrib['id']
+
+"""
 artist
 """
-
+for child in ampache.artist(ampache_url, ampache_api, single_artist, ''):
+    if child.tag == 'artist':
+        print('\nsearching for an artist with this id', single_artist)
+        print(child.tag, child.attrib)
 """
 artist_albums
 """
-
+print('\nsearching for albums that are in this artist', single_artist)
+for child in ampache.artist_albums(ampache_url, ampache_api, single_artist, '', 0):
+    if child.tag == 'album':
+        print(child.tag, child.attrib)
 """
 artist_songs
 """
-
+print('\nsearching for songs that are in this artist', single_artist)
+for child in ampache.artist_songs(ampache_url, ampache_api, single_artist, '', 0):
+    if child.tag == 'song':
+        print(child.tag, child.attrib)
 """
 artists
 """
@@ -181,10 +199,6 @@ song
 
 """
 songs
-"""
-
-"""
-stats
 """
 
 """
