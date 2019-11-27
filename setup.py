408766a7009ceddd2396d73747815a6ee14b8bf6
lachlan <lachlan.00@gmail.com>
2019-11-27 14:37:27 +1000

    src again

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open(path.join(_here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="ampache",
-	packages=find_packages(where='src'),
+	py_modules=["ampache"]
     package_dir={'': 'src'},
     author="Lachlan de Waard (lachlan-00)",
     author_email="lachlan.00@gmail.com",
